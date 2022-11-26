ad148343e75868350fc3f6304e92e447fbf1312e
Alexander Schilling <dignityinside@protonmail.com>
2022-11-26 18:05:19 +0100

    Remove example code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="home">
+    <h1>Home</h1>
+  </div>
   </main>
 </template>
