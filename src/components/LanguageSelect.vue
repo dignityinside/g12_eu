e81cb2725e43140c65afbfba64bf1c40d8ba2897
Alexander Schilling <dignityinside@protonmail.com>
2022-11-27 13:53:33 +0100

    Update language on change

--- src/components/LanguageSelect.vue
+++ src/components/LanguageSelect.vue
@@ -1,17 +1,20 @@
 <script setup>
-  import { ref } from 'vue';
+  import i18n from '../i18n';
   
-  const languages = ref(['ru', 'de', 'ua', 'en']);
+  // set initial language
+  languageChanged();
+
+  // update on change language
+  function languageChanged() {
+    document.querySelector('html').setAttribute('lang', i18n.global.locale);
+    document.title = i18n.global.t('site.name')
+  }
 </script>
 
 <template>
-  <select v-model="$i18n.locale">
-    <option v-for="(language, index) in languages" :key="`language-${index}`" :value="language">
+  <select v-model="$i18n.locale" @change="languageChanged()">
+    <option v-for="(language, index) in $i18n.availableLocales" :key="`language-${index}`" :value="language">
       {{ language }}
     </option>
   </select>
 </template>
-
-<style scoped>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
-import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
-import language from './language'
+import i18n from './i18n'
 
 const app = createApp(App)
-
-const i18n = createI18n(language)
 
 app.use(i18n)
 
